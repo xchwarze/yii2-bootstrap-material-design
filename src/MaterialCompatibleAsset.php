9af4181f8afe150612ca3ea5ada5777fdeaf6a43
DSR! <xchwarze@gmail.com>
2023-10-03 19:51:22 -0300

    Fix css bugs

--- src/MaterialCompatibleAsset.php
+++ src/MaterialCompatibleAsset.php
@@ -11,9 +11,15 @@ class MaterialCompatibleAsset extends MaterialAsset
     {
         parent::publish($am);
 
+        // I don't understand why they had the idea to break compatibility with the original bootstrap...
         $filePath = $am->getAssetPath($this, 'js/mdb.min.js');
         if (file_exists($filePath)) {
-            // I don't understand why they had the idea to break compatibility with the original bootstrap...
+            $content = str_replace('data-mdb-', 'data-bs-', file_get_contents($filePath));
+            file_put_contents($filePath, $content);
+        }
+
+        $filePath = $am->getAssetPath($this, 'css/mdb.min.css');
+        if (file_exists($filePath)) {
             $content = str_replace('data-mdb-', 'data-bs-', file_get_contents($filePath));
             file_put_contents($filePath, $content);
         }
