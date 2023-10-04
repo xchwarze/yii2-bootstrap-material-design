cc575bc074d124291c5ef4a7547561444a56ac94
DSR! <xchwarze@gmail.com>
2023-10-03 21:37:11 -0300

    Fix ActiveField with external widgets

--- src/widgets/ActiveField.php
+++ src/widgets/ActiveField.php
@@ -53,53 +53,9 @@ class ActiveField extends \yii\bootstrap5\ActiveField
      */
     protected function createLayoutConfig(array $instanceConfig): array
     {
-        $config = [
-            'hintOptions' => [
-                'tag' => 'div',
-                'class' => ['form-text', 'text-muted'],
-            ],
-            'errorOptions' => [
-                'tag' => 'div',
-                'class' => 'invalid-feedback',
-            ],
-            'labelOptions' => [
-                'class' => ['form-label'],
-            ],
-        ];
-
-        $layout = $instanceConfig['form']->layout;
-        if ($layout === \yii\bootstrap5\ActiveForm::LAYOUT_HORIZONTAL) {
-            $config['template'] = "{label}\n{beginWrapper}\n{input}\n{error}\n{hint}\n{endWrapper}";
-            $config['wrapperOptions'] = [];
-            $config['labelOptions'] = [];
-            $config['options'] = [];
-            $cssClasses = [
-                'offset' => ['col-sm-10', 'offset-sm-2'],
-                'label' => ['col-sm-2', 'col-form-label'],
-                'wrapper' => 'col-sm-10',
-                'error' => '',
-                'hint' => '',
-                'field' => 'mb-3 row',
-            ];
-            if (isset($instanceConfig['horizontalCssClasses'])) {
-                $cssClasses = ArrayHelper::merge($cssClasses, $instanceConfig['horizontalCssClasses']);
-            }
-            $config['horizontalCssClasses'] = $cssClasses;
-
-            Html::addCssClass($config['wrapperOptions'], $cssClasses['wrapper']);
-            Html::addCssClass($config['labelOptions'], $cssClasses['label']);
-            Html::addCssClass($config['errorOptions'], $cssClasses['error']);
-            Html::addCssClass($config['hintOptions'], $cssClasses['hint']);
-            Html::addCssClass($config['options'], $cssClasses['field']);
-        } elseif ($layout === \yii\bootstrap5\ActiveForm::LAYOUT_INLINE) {
-            $config['inputOptions']['placeholder'] = true;
-            $config['enableError'] = false;
-
-            Html::addCssClass($config['labelOptions'], ['screenreader' => 'visually-hidden']);
-        } elseif ($layout === \yii\bootstrap5\ActiveForm::LAYOUT_FLOATING) {
-            $config['inputOptions']['placeholder'] = true;
-            $config['template'] = "{input}\n{label}\n{error}\n{hint}";
-            Html::addCssClass($config['options'], ['layout' => 'form-floating mt-3']);
+        $config = parent::createLayoutConfig($instanceConfig);
+        if (isset($config['inputOptions'])) {
+            unset($config['inputOptions']);
         }
 
         return $config;
